ecefa52ed52b0aaf3fdcd2768e37f0839df8a268
hashkitti001 <iJeddy5.0@gmail.com>
2024-05-14 09:40:25 +0100

    add functionality to get article by id

--- blog_api/controllers/articleController.js
+++ blog_api/controllers/articleController.js
@@ -4,18 +4,35 @@ let getAllArticles = async (req, res) => {
     try {
         const articles = await Article.find({})
         if (articles) {
-            return res.status(200).json({ "articles": articles })
+            return res.status(200).json({ articles })
         }
         else {
             throw new Error("No articles found")
         }
     } catch (e) {
-        console.error("Couldn't get articles because", e.msg)
+        console.error("Couldn't get articles because", e.message)
         res.status(500).json({error: "Couldn't get articles"})
+    }
+
+}
+let getArticleById = async(req, res) => {
+    try {
+        let id = req.params.id
+        const article = await Article.findById(id)
+        if (article) {
+            return res.status(200).json({ "article": article })
+        }
+        else {
+            throw new Error("No articles found")
+        }
+    } catch (e) {
+        console.error("Couldn't get article by that id", e.message)
+        res.status(500).json({error: "Couldn't get article by that id"})
     }
 
 }
 
 module.exports = {
-    getAllArticles
+    getAllArticles, getArticleById,
+
 }
