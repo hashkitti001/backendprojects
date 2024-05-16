3e96b03918c448941eb336af37dc0d10a17d9e6b
hashkitti001 <iJeddy5.0@gmail.com>
2024-05-16 09:12:34 +0100

    add task model definition

---
+++ todo_api/models/task.js
@@ -0,0 +1,39 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const User = require("../models/user")
+const { db } = require("../utils/dbConnect")
+
+const Task = db.define(
+    'Task',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        dueDate: {
+            type: DataTypes.DATE,
+            allowNull: true
+        },
+        status: {
+            type: DataTypes.ENUM("undone", "pending", "done"),
+            allowNull: false,
+            defaultValue: "undone"
+        },
+        priority: { 
+            type: DataTypes.ENUM("low", "medium", "high"),
+            defaultValue: "low", // Corrected from default to defaultValue
+            allowNull: true
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+        }
+    }
+);
+Task.belongsTo(User)
+module.exports = Task;
