6744e7a30db6e02c5658b998cc05aabe898ec3ac
hashkitti001 <iJeddy5.0@gmail.com>
2024-05-16 09:14:18 +0100

    add additional setup to app.js

--- todo_api/app.js
+++ todo_api/app.js
@@ -1,8 +1,18 @@
+const cookieParser = require("cookie-parser")
+const logger = require("morgan")
 const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 3000
 const dbConnect = require("./utils/dbConnect")
+const indexRouter = require("./routes")
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+// app.use(indexRouter)
+logger("dev")
+/* Initiate db connection */
 dbConnect()
+
 app.listen(PORT, () => {
-    console.log("Todo API listening on port", PORT)
+    console.log("Todo API listening on port 3000")
 })
