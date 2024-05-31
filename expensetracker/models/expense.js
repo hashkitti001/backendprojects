a4bd8347f06425f578e23287f07b72c6b35b76a1
hashkitti001 <iJeddy5.0@gmail.com>
2024-05-31 16:08:42 +0100

    add expense model

---
+++ expensetracker/models/expense.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose")
+const { Schema } = mongoose
+const expenseSchema = new Schema({
+    "description": {
+        type: String,
+        required: true,
+    },
+    "amount": {
+        type: Number,
+        required: true
+    },
+    "price": {
+        type: Number,
+        required: true
+    },
+    "category": {
+        type: String,
+        enum: ['Groceries', 'Leisure', 'Electronics', 'Utilities', 'Clothing'],
+        required: true
+    },
+    "date": {
+        type: Date,
+        default: new Date()
+            .toJSON()
+            .substring(0, 10),
+        required: true
+    },
+    "userId": {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+})
+const Expense = mongoose.model("Expenses", expenseSchema)
+module.exports = Expense
