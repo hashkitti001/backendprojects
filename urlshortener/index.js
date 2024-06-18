9a067cae115b5390f9644be209a6e29f0f475fa5
hashkitti001 <iJeddy5.0@gmail.com>
2024-06-18 12:28:56 +0100

    bulk upload url shortener task

---
+++ urlshortener/index.js
@@ -0,0 +1,74 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const mongoose = require("mongoose")
+const Url = require("./urlModel")
+const validator = require("validator")
+
+// Basic Configuration
+const port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors());
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+//Connects to the database
+let dbConn = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log("Connected successfully")
+
+  }
+  catch (e) {
+    console.log(e)
+  }
+}
+dbConn()
+app.get('/api/shorturl/:id', async (req, res) => {
+
+  try {
+    let foundURL = await Url.findOne({ identifier: req.params.id })
+    res.redirect(foundURL.longurl)
+  } catch (e) {
+    console.error("Something went wrong", e)
+    res.status(500).json({ message: "Internal server error" })
+  }
+})
+app.post('/api/shorturl', async (req, res) => {
+  try {
+    let { url } = req.body
+    if (!validator.isURL(url)) {
+      return res.status(200).json({ message: "Invalid URL" })
+    }
+    else {
+      const previousURLs = await Url.find({})
+      const urlCount = previousURLs.length
+      const newURL = new Url({
+        identifier: urlCount,
+        longurl: req.body.url
+      })
+      await newURL.save()
+      res.status(201).json({
+        original_url: req.body.url,
+        short_url: urlCount
+      })
+
+    }
+
+
+  }
+
+  catch (e) {
+    res.status(500).json({ message: "Internal Server Error" })
+    console.error(e)
+  }
+});
+
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
+});
